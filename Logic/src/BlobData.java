6deadd6d3d93183629edc473286fd43d19bf9a32
Yair Zvuloon <yairzv0@gmail.com>
2019-07-30 19:11:23 +0300

    added external lib for sha1

--- Logic/src/BlobData.java
+++ Logic/src/BlobData.java
@@ -5,85 +5,76 @@ public class BlobData {
     private String m_Name;
     private String m_SHA1;
     private String m_Type;
+    private Integer m_Id;
     private String m_LastChangedBY;
     private SimpleDateFormat m_LastChangedTime;
-    private int m_HashCode;
+
 
     public BlobData(
             String i_Name,
             String i_SHA1,
             String i_Type,
             String i_LastChangedBY,
+            String i_Id,
             SimpleDateFormat i_LastChangedTime
     ) {
-        this.m_Name = i_Name;
-        this.m_SHA1 = i_SHA1;
-        this.m_Type = i_Type;
-        this.m_LastChangedBY = i_LastChangedBY;
-        this.m_LastChangedTime = i_LastChangedTime;
+        m_Name = i_Name;
+        m_SHA1 = i_SHA1;
+        m_Type = i_Type;
+        m_LastChangedBY = i_LastChangedBY;
+        m_LastChangedTime = i_LastChangedTime;
+        m_Id = Integer.parseInt(i_Id);
+
     }
 
-    public String getM_Name() {
+    public Integer getId() {
+        return m_Id;
+    }
+
+    public String getName() {
         return m_Name;
-    }
-
-    public void setName(String m_Name) {
-        this.m_Name = m_Name;
     }
 
     public String getSHA1() {
         return m_SHA1;
     }
 
-    public void setSHA1(String m_SHA1) {
-        this.m_SHA1 = m_SHA1;
-    }
-
     public String getType() {
         return m_Type;
-    }
-
-    public void setType(String m_Type) {
-        this.m_Type = m_Type;
     }
 
     public String getLastChangedBY() {
         return m_LastChangedBY;
     }
 
-    public void setLastChangedBY(String m_LastChangedBY) {
-        this.m_LastChangedBY = m_LastChangedBY;
-    }
-
-    public SimpleDateFormat getM_LastChangedTime() {
+    public SimpleDateFormat getLastChangedTime() {
         return m_LastChangedTime;
     }
 
-    public void setLastChangedTime(SimpleDateFormat m_LastChangedTime) {
-        this.m_LastChangedTime = m_LastChangedTime;
-    }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BlobData blobData = (BlobData) o;
-        return m_Name.equals(blobData.m_Name) &&
-                m_SHA1.equals(blobData.m_SHA1) &&
-                m_Type.equals(blobData.m_Type) &&
-                m_LastChangedBY.equals(blobData.m_LastChangedBY) &&
-                m_LastChangedTime.equals(blobData.m_LastChangedTime);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        BlobData blobData = (BlobData) o;
+//        return m_Name.equals(blobData.m_Name) &&
+//                m_SHA1.equals(blobData.m_SHA1) &&
+//                m_Type.equals(blobData.m_Type) &&
+//                m_LastChangedBY.equals(blobData.m_LastChangedBY) &&
+//                m_LastChangedTime.equals(blobData.m_LastChangedTime);
+//    }
 
-    @Override
-    public int hashCode() {
-        if(m_HashCode==0)
-        {
-            m_HashCode= Objects.hash(m_Name, m_SHA1, m_Type, m_LastChangedBY, m_LastChangedTime);
-        }
-        return m_HashCode;
-    }
+//    @Override
+//    public int hashCode() {
+//        if(m_HashCode==0)
+//        {
+//            m_HashCode= Objects.hash(m_Name, m_SHA1, m_Type, m_LastChangedBY, m_LastChangedTime);
+//        }
+//        return m_HashCode;
+//    }
+//
+//    public String
 
     @Override
     public String toString() {
