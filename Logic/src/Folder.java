407db72cc5fec79c8b20424c9a9eaec10cae0e58
Yair Zvuloon <yairzv0@gmail.com>
2019-07-30 18:15:11 +0300

    fix the structure

--- Logic/src/Folder.java
+++ Logic/src/Folder.java
@@ -1,18 +1,22 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Folder {
-   private Set<BlobData> m_Files=new HashSet<>();
 
-    public Set<BlobData> getFiles() {
-        return m_Files;
-    }
+   private HashMap<Integer,BlobData> m_Files=new HashMap<>();
 
-    //public getByName(String i_Name)
+
+//    public Set<BlobData> getFiles() {
+//        return m_Files;
+//    }
+//
+//    //public getByName(String i_Name)
+//
 
     public void addFileToFolder(BlobData i_BlobData) {
-        m_Files.add(i_BlobData);
-    }
+        m_Files.put(i_BlobData.getId(),i_BlobData);
+}
 
 
 }
